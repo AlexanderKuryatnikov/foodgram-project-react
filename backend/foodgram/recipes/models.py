cb08bf35586b2bc3c1031f9966b98ce6590937f6
Alexander Kuryatnikov <asander1995@gmail.com>
2022-06-18 03:30:59 +0300

    added verbose name

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -8,76 +8,107 @@ User = get_user_model()
 
 class Tag(models.Model):
     name = models.CharField(
+        'Название',
         max_length=50,
         unique=True,
     )
     color = models.CharField(
+        'Цвет',
         max_length=7,
         unique=True,
     )
     slug = models.SlugField(
+        'Слаг',
         unique=True,
     )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
 
 
 class Ingridient(models.Model):
     name = models.CharField(
+        'Название',
         max_length=256,
         unique=True,
     )
     measurement_unit = models.CharField(
+        'Единицы измерения',
         max_length=20,
     )
 
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(
+        'Название',
+        max_length=200
+    )
     author = models.ForeignKey(
         User,
+        verbose_name='Автор',
         on_delete=models.CASCADE,
         related_name='recipes'
     )
     pub_date = models.DateTimeField(
+        'Дата публикации',
         auto_now_add=True,
     )
     tags = models.ManyToManyField(
         Tag,
+        verbose_name='Теги',
         related_name='recipes',
     )
     ingridients = models.ManyToManyField(
         Ingridient,
+        verbose_name='Ингредиенты',
         through='IngridientAmount',
         related_name='recipes',
     )
     image = models.ImageField(
+        'Картинка',
         upload_to='recipes/'
     )
-    text = models.TextField()
+    text = models.TextField(
+        'Описание',
+    )
     cooking_time = models.PositiveIntegerField(
+        'Время приготовления',
         validators=(validate_time,)
     )
 
     class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
         ordering = ['-pub_date']
 
 
 class IngridientAmount(models.Model):
     ingridient = models.ForeignKey(
         Ingridient,
+        verbose_name='Ингредиент',
         on_delete=models.CASCADE,
         related_name='ingridient_amount'
     )
     recipe = models.ForeignKey(
         Recipe,
+        verbose_name='Рецепт',
         on_delete=models.CASCADE,
         related_name='ingridient_amount'
     )
     amount = models.DecimalField(
+        'Количество',
         max_digits=6,
         decimal_places=2,
     )
 
     class Meta:
+        verbose_name = 'Количество ингредиентов'
+        verbose_name_plural = 'Количество ингредиентов'
         constraints = [
             models.UniqueConstraint(
                 fields=['ingridient', 'recipe'],
@@ -89,16 +120,20 @@ class IngridientAmount(models.Model):
 class Favorite(models.Model):
     user = models.ForeignKey(
         User,
+        verbose_name='Пользователь',
         on_delete=models.CASCADE,
         related_name='favorites',
     )
     recipe = models.ForeignKey(
         Recipe,
+        verbose_name='Рецепт',
         on_delete=models.CASCADE,
         related_name='favorites'
     )
 
     class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'recipe'],
@@ -110,16 +145,20 @@ class Favorite(models.Model):
 class ShoppingCart(models.Model):
     user = models.ForeignKey(
         User,
+        verbose_name='Пользователь',
         on_delete=models.CASCADE,
         related_name='shopping_cart',
     )
     recipe = models.ForeignKey(
         Recipe,
+        verbose_name='Рецепт',
         on_delete=models.CASCADE,
         related_name='shopping_cart'
     )
 
     class Meta:
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'recipe'],
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -6,16 +6,28 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(
+        'Электронная почта',
         max_length=254,
         unique=True,
     )
     username = models.CharField(
+        'Юзернейм',
         max_length=150,
         unique=True,
         validators=[UnicodeUsernameValidator()],
     )
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
 
 User = get_user_model()
@@ -24,16 +36,20 @@ User = get_user_model()
 class Subscribtion(models.Model):
     user = models.ForeignKey(
         User,
+        verbose_name='Пользователь',
         on_delete=models.CASCADE,
         related_name='subscriber'
     )
     subscribed = models.ForeignKey(
         User,
+        verbose_name='Подписан на',
         on_delete=models.CASCADE,
         related_name='subscribed'
     )
 
     class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'subscribed'],
